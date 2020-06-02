07ddc9a52fd4b0a1639857b5b464bece685ebf49
jung-seok Cho <jcho7178@gmail.com>
2020-06-03 00:08:27 +0900

    [add files] 강의 App- 글목록 출력하기

---
+++ backend/nodejs/syntax/array-loop.js
@@ -0,0 +1,9 @@
+var number = [1,400,12,34,5];
+var total = 0;
+var i = 0;
+while(i<number.length){
+  console.log(number[i]);
+  total = total + number[i];
+  i=i+1;
+}
+console.log(total);
---
+++ backend/nodejs/syntax/array.js
@@ -0,0 +1,8 @@
+var arr = ['a', 'b', 'c', 'd'];
+console.log(arr);
+
+console.log(arr.length);
+
+arr.push('e');
+
+console.log(arr);
